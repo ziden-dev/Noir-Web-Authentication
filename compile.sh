ff8e9e26910b2698363c1dda74f4ec9249fb1173
admin <admin@admins-MacBook-Air.local>
2023-09-18 14:47:32 +0700

    merkle tree

--- compile.sh
+++ compile.sh
@@ -4,3 +4,8 @@ cd src/circuits/authentication
 echo "Compiling the authentication circuit..."
 nargo compile
 echo "Successfully compiled the authentication circuit"
+cd ../../../
+cd src/circuits/merkleTree
+echo "Compiling the merkleTree circuit..."
+nargo compile 
+echo "Successfully compiled the merkle tree circuit"
---
+++ test/merkleTree-chung.ts
@@ -0,0 +1,58 @@
+import { CryptographyPrimitives } from "../src/crypto/index.js";
+import { expect } from "chai";
+import { buildPoseidon } from "../src/crypto/poseidon_wasm.js";
+import { NormalMerkleTree } from "../src/scripts-merkleTree/tree/normal-merkle-tree.js";
+
+import circuit from "../src/circuits/merkleTree/target/merkleTree.json" assert { type: "json" };
+import { convertToHexAndPad } from "../src/utils/bits.js";
+import { validateWitness } from "../src/berretenberg-api/index.js";
+
+describe("test merkle tree ", () => {
+  let poseidon: any;
+  let crypto: CryptographyPrimitives;
+
+  let merkleTree: any;
+  before(async () => {
+    crypto = await CryptographyPrimitives.getInstance();
+    poseidon = await buildPoseidon();
+  });
+  it("poseidon", async () => {
+    const res = poseidon([1, 2]);
+    expect(poseidon.F.toString(res)).equal(
+      BigInt(
+        "0x115cc0f5e7d690413df64c6b9662e9cf2a3617f2743245519e19607a4417189a"
+      ).toString(10)
+    );
+  });
+
+  it(" Create Normal Merkle", async () => {
+    merkleTree = new NormalMerkleTree(3, poseidon);
+    merkleTree.insert(1n);
+    merkleTree.insert(2n);
+    merkleTree.insert(3n);
+    merkleTree.insert(4n);
+
+    console.log("root = ", merkleTree.getRoot());
+
+    let proof = merkleTree.getPathProof(1);
+
+    let inputs = [
+      proof.value,
+      proof.index,
+      ...proof.path,
+      merkleTree.getRoot(),
+    ];
+
+    console.log(inputs);
+
+    const witness = new Map<number, string>();
+    inputs.forEach((input, index) => {
+      witness.set(index + 1, convertToHexAndPad(input));
+    });
+
+    console.log(witness);
+
+    const isWitnessValid = await validateWitness(witness, circuit);
+    expect(isWitnessValid).to.be.true;
+  });
+});
